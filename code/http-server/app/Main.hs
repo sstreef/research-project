4d1c61ed04c2c8734c4bd26da22e71c3e18ec952
Sam Streef <s.l.streef@student.tudelft.nl>
2022-06-13 15:12:39 +0200

    Added static file system options

--- code/http-server/app/Main.hs
+++ code/http-server/app/Main.hs
@@ -8,13 +8,13 @@ import HTTP.General (Payload(..))
 main :: IO ()
 main = runWith Nothing [
     ((GET, "/person"), \_ -> 
-        createJSONResponse (Just OK) "{ \"Name\": \"Bob\", \"Age\": 27, \"Gender\": \"Male\" }"),
+        return $ createJSONResponse (Just OK) "{ \"Name\": \"Bob\", \"Age\": 27, \"Gender\": \"Male\" }"),
 
     ((GET, "/hello"), \_ -> 
-        createPlainResponse (Just OK) "The Quick Brown Fox Jumps Over The Lazy Dog."),
+        return $ createPlainResponse (Just OK) "The Quick Brown Fox Jumps Over The Lazy Dog."),
 
     ((POST, "/mirror"), \(Request _ payload) -> 
-                case payload of
+                return $ case payload of
                     Payload _ t c -> createContentResponse (Just t) (Just OK) c
                     Empty         -> badRequestResponse)
-    ] Nothing
+    ] (Just "./resources/")
--- code/http-server/src/Server.hs
+++ code/http-server/src/Server.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Server ( runWith ) where
 
 import Prelude hiding (log)
@@ -9,8 +10,8 @@ import Control.Concurrent (forkFinally)
 
 import qualified Control.Exception as E
 import Data.Maybe (fromMaybe)
-import HTTP.Request (HTTPRequest (Request), MethodType, HTTPHeaders (Headers), Meta (Meta, None), getHeaders, getMeta)
-import HTTP.Response (HTTPResponse (HTTPResponse), createPlainResponse, Status (OK, NotFound), createStatusResponse, badRequestResponse, notFoundResponse)
+import HTTP.Request (HTTPRequest (Request), MethodType (GET), HTTPHeaders (Headers), Meta (Meta, None), getHeaders, getMeta)
+import HTTP.Response (HTTPResponse (HTTPResponse), createPlainResponse, Status (OK, NotFound), createStatusResponse, badRequestResponse, notFoundResponse, createContentResponse)
 import Network.Socket.ByteString ( recv, sendAll )
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Char8 as C
@@ -19,8 +20,10 @@ import Data.Functor ((<&>))
 import Data.Either (fromLeft)
 import HTTP.General (Payload(Empty))
 import Control.Monad.State (execState)
+import Text.Read (Lexeme(String))
+import Parsers.File
 
-type Handler = HTTPRequest -> HTTPResponse
+type Handler = HTTPRequest -> IO HTTPResponse
 
 type Port = String
 
@@ -29,36 +32,51 @@ runWith port handlers staticFilePath = runTCPServer Nothing (fromMaybe "3000" po
   where
     bytesToRead = 128
 
-    logRequest :: HTTPRequest -> IO ()
-    logRequest (Request (Headers (Meta m p v) _) _) = putStrLn (show m ++ " " ++ p ++ " " ++ v)
-    logRequest _ = putStrLn "Invalid request"
-
-    createSocketHandler :: Socket -> IO () 
+    createSocketHandler :: Socket -> IO ()
     createSocketHandler  = bufferedSocketHandle (return emptyRequest)
 
-    lookupRequestHandler :: (MethodType, String) -> Maybe Handler
-    lookupRequestHandler key = lookup key handlers 
+    handleFileRequest :: (MethodType, String) -> Handler
+    handleFileRequest (GET, path) request = case staticFilePath of
+        Just rootPath -> readFromFile rootPath path
+        Nothing       -> return HTTP.Response.notFoundResponse
+    handleFileRequest (_, _) _ = return HTTP.Response.badRequestResponse
 
     handleRequest :: BufferState -> IO HTTPResponse
-    handleRequest state = 
+    handleRequest state =
       let (_, mode, request) = execState state emptyState
-      in do 
+      in do
         logRequest request
-        return $ case mode of
-          ParsingFinished -> case getMeta request >>= lookupRequestHandler of
-            Just handler    -> handler request
-            Nothing         -> notFoundResponse
-          _             -> badRequestResponse
+        case mode of
+          ParsingFinished -> fromMaybe
+            (return badRequestResponse) $ do
+              key <- getMeta request
+              return (fromMaybe (handleFileRequest key) (lookup key handlers) request)
+          _             -> return badRequestResponse
 
     bufferedSocketHandle :: BufferState -> Socket ->  IO ()
     bufferedSocketHandle oldState sock = do
       bytes <- recv sock bytesToRead
 
       let newState = do { oldState; addBytes bytes; consume }
-      
+
       if S.null bytes || S.length bytes < bytesToRead
             then handleRequest newState >>= sendAll sock . C.pack . show
             else bufferedSocketHandle newState sock
+
+
+{- Helpers for server function -}
+readFromFile :: String -> String -> IO HTTPResponse
+readFromFile rootPath path = do
+  (eitherErrorOrString :: Either IOError String) <- E.try $ readFile (rootPath ++ path)
+  return $ case eitherErrorOrString of
+    Right content -> createContentResponse (getFileContentType path) (Just OK) content
+    Left _        -> HTTP.Response.notFoundResponse
+
+logRequest :: HTTPRequest -> IO ()
+logRequest (Request (Headers (Meta m p v) _) _) = putStrLn (show m ++ " " ++ p ++ " " ++ v)
+logRequest _ = putStrLn "Invalid request"
+
+{- Boilerplate code from the Network library -}
 
 runTCPServer :: Maybe HostName -> ServiceName -> (Socket -> IO ()) -> IO ()
 runTCPServer mhost port server = withSocketsDo $ do
