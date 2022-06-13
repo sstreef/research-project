73408413f62ecd6632336d0787b8d5266a992782
Sam Streef <s.l.streef@student.tudelft.nl>
2022-06-13 15:25:29 +0200

    Added better handler examples

--- code/http-server/app/Main.hs
+++ code/http-server/app/Main.hs
@@ -1,20 +1,32 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Server (runWith)
 import HTTP.Request (MethodType(GET, POST), HTTPRequest (Request))
 import HTTP.Response ( Status(OK), createJSONResponse, createPlainResponse, createContentResponse, badRequestResponse )
 import HTTP.General (Payload(..))
+import Data.Char (toUpper)
 
 main :: IO ()
 main = runWith Nothing [
-    ((GET, "/person"), \_ -> 
+    ((GET, "/person"), \_ ->
         return $ createJSONResponse (Just OK) "{ \"Name\": \"Bob\", \"Age\": 27, \"Gender\": \"Male\" }"),
 
-    ((GET, "/hello"), \_ -> 
+    ((GET, "/fox"), \_ ->
         return $ createPlainResponse (Just OK) "The Quick Brown Fox Jumps Over The Lazy Dog."),
 
-    ((POST, "/mirror"), \(Request _ payload) -> 
+    ((POST, "/caps"), \(Request _ payload) -> do
                 return $ case payload of
-                    Payload _ t c -> createContentResponse (Just t) (Just OK) c
+                    Payload _ t c -> createContentResponse (Just t) (Just OK) (map toUpper (content payload))
+                    Empty         -> badRequestResponse),
+    
+    ((POST, "/add"), \(Request _ payload) -> do
+                return $ case payload of
+                    Payload _ t c -> createContentResponse (Just t) (Just OK) (show (sum $ map read (words (content payload))))
+                    Empty         -> badRequestResponse),
+
+    ((POST, "/reverse"), \(Request _ payload) -> do
+                return $ case payload of
+                    Payload _ t c -> createContentResponse (Just t) (Just OK) (reverse c)
                     Empty         -> badRequestResponse)
     ] (Just "./resources/")
